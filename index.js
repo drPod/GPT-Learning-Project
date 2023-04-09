15b541b2393ae60e6b292e3d79e40d5905d58140
Darsh Poddar <darshpoddar@Darshs-MacBook-Pro.local>
2023-04-09 18:23:50 -0500

    Third commit

--- index.js
+++ index.js
@@ -33,4 +33,81 @@ openai.createCompletion({
     console.log(error);
   }); */
 
+  /*
+  const { Configuration, OpenAIApi } = require("openai");
+
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
   
+  const openai = new OpenAIApi(configuration);
+  
+  openai.createCompletion({
+    engine: "text-davinci-002",
+    prompt: "Once upon a time, there was a brave knight",
+    maxTokens: 64,
+    n: 1,
+    stop: "\n",
+    temperature: 0.5,
+  }).then((response) => {
+    console.log(response.data.choices[0].text);
+  }).catch((error) => {
+    console.log(error);
+  });
+  */
+
+/*
+THE FOLLOWING CODE OUTPUTS THE FOLLOWING: {object: 'list', data: Array(50)} I STILL HAVE NO CLUE WHAT THIS MEANS
+
+
+  const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: "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"
+});
+
+const openai = new OpenAIApi(configuration);
+
+openai.listEngines()
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+*/
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+// define function to get number of available engines
+async function getNumEngines() {
+  try {
+    const response = await openai.listEngines();
+    console.log(response.data)
+    return response.data.data.length;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// loop through and output all the models
+async function loopEngines() {
+  const numEngines = await getNumEngines();
+  for (let i = 0; i < numEngines; i++) {
+    try {
+      const response = await openai.listEngines();
+      console.log(response.data.data[i].id);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+loopEngines();
+
