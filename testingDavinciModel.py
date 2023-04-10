5f2a546787d1aa0e94328ee86c32b48e3da062a3
Darsh Poddar <darshpoddar@Darshs-MacBook-Pro.local>
2023-04-09 19:40:21 -0500

    First Commit

---
+++ testingDavinciModel.py
@@ -0,0 +1,17 @@
+# I'm going to try testing a GPT model in Python to see if it gives me a more detailed error message because it's getting frustrating seeing the status 400 over and over again
+
+import os
+import openai
+
+openai.api_key = os.getenv("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")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread\nOutput: send-msg `find constance` Do we need some bread?\n\nReach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday",
+  temperature=0,
+  max_tokens=100,
+  top_p=1.0,
+  frequency_penalty=0.2,
+  presence_penalty=0.0,
+  stop=["\n"]
+)
