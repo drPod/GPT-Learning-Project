a091813d9f1500865889664b5fcbeb1e286adb7e
Darsh Poddar <darshpoddar@Darshs-MacBook-Pro.local>
2023-04-09 20:34:48 -0500

    Second Commit

--- testingDavinciModel.py
+++ testingDavinciModel.py
@@ -3,15 +3,16 @@
 import os
 import openai
 
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv(
+    "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")
 
 response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread\nOutput: send-msg `find constance` Do we need some bread?\n\nReach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday",
-  temperature=0,
-  max_tokens=100,
-  top_p=1.0,
-  frequency_penalty=0.2,
-  presence_penalty=0.0,
-  stop=["\n"]
+    model="text-davinci-003",
+    prompt="Convert this text to a programmatic command:\n\nExample: Ask Constance if we need some bread\nOutput: send-msg `find constance` Do we need some bread?\n\nReach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday",
+    temperature=0,
+    max_tokens=100,
+    top_p=1.0,
+    frequency_penalty=0.2,
+    presence_penalty=0.0,
+    stop=["\n"]
 )
